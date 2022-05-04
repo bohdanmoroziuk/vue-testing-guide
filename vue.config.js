7687f112c69c5be6022856b3ddfec9d32e9102b9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-04 15:36:37 +0300

    chore: set proxy

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://jsonplaceholder.typicode.com',
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
 });
