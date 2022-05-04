829e019d47da2e350ceb2e9995f81e6c3b41a9fc
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-04 12:50:37 +0300

    chore: add test todos

---
+++ src/components/02_test-props/AppHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Props {
+  title: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <header class="app-header">
+    <h1 class="title">
+      {{ title }}
+    </h1>
+  </header>
+</template>
---
+++ tests/unit/02_props/AppHeader.spec.ts
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+
+import AppHeader from '../../../src/components/02_test-props/AppHeader.vue';
+
+describe('AppHeader', () => {
+  const mockTitle = 'Test Vue Apps with Vue Test Utils';
+
+  it.todo('renders successfully');
+
+  it.todo('renders a title');
+});
