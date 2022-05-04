dcc8e1f73dc606321570119f6d926876aadf8037
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-04 13:52:55 +0300

    chore: add test todos

---
+++ src/components/03_conditional-rendering/AppNav.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+
+interface Props {
+  isAdmin: boolean;
+}
+
+defineProps<Props>();
+
+const shouldShowDropdown = ref(false);
+
+const toggleDropdown = () => {
+  shouldShowDropdown.value = !shouldShowDropdown.value;
+};
+</script>
+
+<template>
+  <nav>
+    <a id="profile" href="/profile">Profile</a>
+    <a id="admin" href="/admin" v-if="isAdmin">Admin</a>
+    <button @click="toggleDropdown">Actions</button>
+    <ul id="dropdown" v-show="shouldShowDropdown">
+      <li>
+        <button>Logout</button>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ tests/unit/03_conditional-rendering/AppNav.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils';
+
+import AppNav from '../../../src/components/03_conditional-rendering/AppNav.vue';
+
+describe('AppNav', () => {
+  describe('when the user does not have admin rights', () => {
+    it.todo('renders a profile link');
+
+    it.todo('does not render an admin link');
+  });
+
+  describe('when the user has admin rights', () => {
+    it.todo('renders an admin link');
+  });
+
+  describe('dropdown', () => {
+    it.todo('does not show dropdown');
+
+    it.todo('shows dropdown on actions button click');
+  });
+});
