c2e44da938414f789967bd69f36cc17f1ccbee79
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-05-04 12:50:06 +0300

    chore: add test todos

---
+++ src/components/01_render-components/ErrorMessage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>An error has occurred!</p>
+  </div>
+</template>
---
+++ tests/unit/01_render-components/ErrorMessage.spec.ts
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils';
+
+import ErrorMessage from '../../../src/components/01_render-components/ErrorMessage.vue';
+
+describe('ErrorMessage', () => {
+  it.todo('renders successfully');
+
+  it.todo('renders an error message');
+});
